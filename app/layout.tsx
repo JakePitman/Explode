6fff710c7a0b2452af3ba7870153b5e7f1282a10
Jake Pitman <jakeseanp@gmail.com>
2023-09-20 20:09:48 +1200

    Change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "R3F Template",
+  title: "Explode",
   description: "R3F Template, with Next, Tailwind and Typescript",
 };
 
